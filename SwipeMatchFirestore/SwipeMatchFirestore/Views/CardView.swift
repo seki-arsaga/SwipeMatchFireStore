7049ca99358d851b30f4e10ceb530b8b246940b2
丸尾優介 <appdevelop19910925@gmail.com>
2019-03-07 02:31:20 +0900

    2019/02/07

---
+++ SwipeMatchFirestore/SwipeMatchFirestore/Views/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  SwipeMatchFirestore
+//
+//  Created by YusuKe on 2019/03/07.
+//  Copyright © 2019年 YusuKe. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
